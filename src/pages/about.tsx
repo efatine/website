7efb1e6b85dba1d9d2ef931a35e86c434ff86fe5
Elias <efati055@uottawa.ca>
2024-02-04 15:21:24 -0500

    added effects on about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,28 +8,62 @@ import { MDXComponents } from 'components/MDXComponents'
 //@ts-ignore
 import rehypePrism from '@mapbox/rehype-prism'
 import { BlogLayout } from 'layouts/BlogLayout'
+import Typed from 'typed.js';
+import { useEffect, useRef } from 'react'
 
-const postDirectory = path.join(process.cwd(), 'src/data/about')
 
-interface AboutProps { 
-  source: MDXRemoteSerializeResult
+const postDirectory = path.join(process.cwd(), 'src/data/about');
+
+interface AboutProps {
+  source: MDXRemoteSerializeResult;
 }
 
 const About: React.FC<AboutProps> = ({ source }) => {
+  const adjectiveRef = useRef<Typed | null>(null);
+
+  useEffect(() => {
+    adjectiveRef.current = new Typed('.adjective-text', {
+
+      strings: [
+        'innovative', 'analytical', 'detail-oriented',
+        'adaptable', 'collaborative', 'resourceful', 'efficient',
+        'team-oriented', 'communicative', 'curious', 'versatile', 'proactive',
+        'resilient', 'logical', 'methodical', 'open-minded', 'self-motivated',
+        'fast learning', 'quality-focused', 'strategic'
+      ],
+      typeSpeed: 70,
+      backSpeed: 30,
+      loop: true,
+      showCursor: true,
+      shuffle: true,
+    });
+
+    return () => {
+      if (adjectiveRef.current) {
+        adjectiveRef.current.destroy();
+      }
+    };
+  }, []);
+
   return (
-    <BlogLayout title={''} description={''}     
-    >
-       <article className="max-w-none w-full mt-8 prose prose-lg dark:prose-dark">
-        <MDXRemote {...source} components={MDXComponents} />
+    <BlogLayout title={''} description={''}>
+      <div className="flex flex-col items-center justify-center text-5xl font-bold">
+        Elias Fatine
+      </div>
+      <div className="flex flex-row items-center mt-4 justify-center w-full prose prose-lg dark:prose-dark text-xl">
+          Aspiring&nbsp;<span className="adjective-text text-xl font-semibold"></span> Software Engineer
+      </div>
+      <article className="max-w-none w-full mt-8 prose prose-lg dark:prose-dark">
+        <MDXRemote {...source} components={{}} />
       </article>
     </BlogLayout>
-  )
-}
+  );
+};
 
 export async function getStaticProps() {
-  const filePath = path.join(postDirectory, `about.mdx`)
-  const fileContents = fs.readFileSync(filePath, 'utf8')
-  const { content, data } = matter(fileContents)
+  const filePath = path.join(postDirectory, `about.mdx`);
+  const fileContents = fs.readFileSync(filePath, 'utf8');
+  const { content, data } = matter(fileContents);
 
   const mdxSource = await serialize(content, {
     scope: data,
@@ -41,13 +75,13 @@ export async function getStaticProps() {
       ],
       rehypePlugins: [rehypePrism],
     },
-  })
+  });
 
   return {
-    props: {//
+    props: {
       source: mdxSource,
     },
-  }
+  };
 }
 
-export default About
+export default About;
